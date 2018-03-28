13b324ad9a024c0132761b9b7345067d145a429c
Lucas Montano <lucasmontano@Lucass-MacBook-Pro.local>
2018-03-27 21:08:33 -0300

    chaing version code

--- app/src/main/java/org/descartae/android/view/fragments/intro/IntroFragment.kt
+++ app/src/main/java/org/descartae/android/view/fragments/intro/IntroFragment.kt
@@ -7,7 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.fragment_intro.*
+import kotlinx.android.synthetic.main.fragment_intro.view.*
 import org.descartae.android.R
 
 const val ARG_PAGE = "page"
@@ -32,42 +32,42 @@ class IntroFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_intro, container, false)
 
-        button_start.setOnClickListener { mListener?.onStartApp() }
+        view.button_start.setOnClickListener { mListener?.onStartApp() }
 
         when (page) {
             0 -> {
 
-                textView_title.setText(R.string.onboard_title_1)
-                textView_subtitle.setText(R.string.onboard_subtitle_1)
-                Picasso.with(activity).load(R.drawable.onboarding_1).into(imageView_intro)
-                button_start.visibility = View.GONE
+                view.textView_title.setText(R.string.onboard_title_1)
+                view.textView_subtitle.setText(R.string.onboard_subtitle_1)
+                Picasso.with(activity).load(R.drawable.onboarding_1).into(view.imageView_intro)
+                view.button_start.visibility = View.GONE
 
                 return view
             }
             1 -> {
 
-                textView_title.setText(R.string.onboard_title_2)
-                textView_subtitle.setText(R.string.onboard_subtitle_2)
-                Picasso.with(activity).load(R.drawable.onboarding_2).into(imageView_intro)
-                button_start.visibility = View.GONE
+                view.textView_title.setText(R.string.onboard_title_2)
+                view.textView_subtitle.setText(R.string.onboard_subtitle_2)
+                Picasso.with(activity).load(R.drawable.onboarding_2).into(view.imageView_intro)
+                view.button_start.visibility = View.GONE
 
                 return view
             }
             2 -> {
 
-                textView_title.setText(R.string.onboard_title_3)
-                textView_subtitle.setText(R.string.onboard_subtitle_3)
-                Picasso.with(activity).load(R.drawable.onboarding_3).into(imageView_intro)
-                button_start.visibility = View.GONE
+                view.textView_title.setText(R.string.onboard_title_3)
+                view.textView_subtitle.setText(R.string.onboard_subtitle_3)
+                Picasso.with(activity).load(R.drawable.onboarding_3).into(view.imageView_intro)
+                view.button_start.visibility = View.GONE
 
                 return view
             }
             3 -> {
 
-                textView_title.setText(R.string.onboard_title_4)
-                textView_subtitle.setText(R.string.onboard_subtitle_4)
-                Picasso.with(activity).load(R.drawable.onboarding_4).into(imageView_intro)
-                button_start.visibility = View.VISIBLE
+                view.textView_title.setText(R.string.onboard_title_4)
+                view.textView_subtitle.setText(R.string.onboard_subtitle_4)
+                Picasso.with(activity).load(R.drawable.onboarding_4).into(view.imageView_intro)
+                view.button_start.visibility = View.VISIBLE
 
                 return view
             }
